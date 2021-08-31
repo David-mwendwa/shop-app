7c32cbc9dd52728516287fd79dc8bd04c318ee01
David Mwendwa <klatzdavie19@gmail.com>
2021-08-31 06:56:28 +0300

    feat: store product data

--- models/product.js
+++ models/product.js
@@ -18,18 +18,21 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price
   }
 
   save() {
-    getProductsFromFile(products => {
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err)
-      })
-    })
-    fs.readFile(p, (err, fileContent) => {})
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+    fs.readFile(p, (err, fileContent) => {});
   }
 
   static fetchAll(cb) {
